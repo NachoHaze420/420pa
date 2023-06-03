281385745072e5acf863686e1ae7d6b3e5965d19
NachoHaze420 <nachohaze420@gmail.com>
2023-06-02 22:38:40 -0300

    420pa

--- Aprende japones original (logo e inicio de sesion)/script.js
+++ Aprende japones original (logo e inicio de sesion)/script.js
@@ -3,6 +3,9 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const hamburguesa = document.querySelector('.hamburguesa');
+const menu = document.querySelector('.menu');
+const menuCruz = document.querySelector('.menu_cross')
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -18,4 +21,12 @@ btnPopup.addEventListener('click', ()=> {
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
+});
+
+hamburguesa.addEventListener('click', ()=> {
+    menu.classList.add('open');
+});
+
+menuCruz.addEventListener('click', ()=> {
+    menu.classList.remove('open');
 });
